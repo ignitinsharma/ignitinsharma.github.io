2bbf0b3ebfdd3893ae20aa17ab42c8d61a4a8b14
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-28 18:28:41 +0530

    about section done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,10 +18,13 @@ const About = () => {
             <h1 style={{ fontWeight: "600", fontSize: "1.3rem" }}>
               Hi 👋, I'm <h3 className="about_box_h3">Nitin Sharma</h3>
             </h1>
-            <p>
-              I am Full Stack Web Developer and i have knowledge in HTML, CSS,
-              Javascript, REACT, Chakra-ui and i make user-friendly websites.
-            </p>
+            <p>I'm a Full Stack Web developer from Bahadurgarh, Haryana.</p>
+            <div className="QuoteTagDiv"s>
+              <i className="QuoteTag">
+                " I enjoy the challenge of creating something new from scartch,
+                and that's drive me to push my creative imagination. "
+              </i>
+            </div>
             <a href="">
               {/* https://drive.google.com/uc?export=download&id=1Tr4JaRtllSROzZWTEwdR4aAUXlfb5CLR */}
               <button className="DownloadResumeBtn">
@@ -62,7 +65,10 @@ const About = () => {
               <a target="_blank" href="https://github.com/ignitinsharma">
                 <GitHubIcon className="AboutIcons" />
               </a>
-              <a target="_blank" href="https://www.linkedin.com/in/ignitinsharma">
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/ignitinsharma"
+              >
                 <LinkedInIcon className="AboutIcons" />
               </a>
               <a target="_blank" href="https://twitter.com/ignitinsharma">
@@ -75,12 +81,20 @@ const About = () => {
               I am <span>Full Stack Web Developer</span>
             </h2>
             <p>
-              My name is Nitin Sharma, I am from Bahadurgarh, Haryana.
-              Passionate Full Stack Web Developer with skills in the MERN stack,
-              I have learned Full Stack Web Development in Masai school. Skilful
-              in problem-solving and building new things using technology. I
-              look forward to joining a company where I can contribute to
-              individual and company growth..
+              {" "}
+              My name is Nitin Sharma, I am from{" "}
+              <span className="DescriptionSpanTag">Bahadurgarh</span> ,{" "}
+              <span className="DescriptionSpanTag">Haryana</span>. I have
+              knowledge in <span className="DescriptionSpanTag">HTML</span> ,{" "}
+              <span className="DescriptionSpanTag">CSS</span> ,{" "}
+              <span className="DescriptionSpanTag">Javascript</span>,{" "}
+              <span className="DescriptionSpanTag">REACT</span>,{" "}
+              <span className="DescriptionSpanTag">Redux</span>,
+              <span className="DescriptionSpanTag">Chakra-Ui</span> and i make
+              user-friendly websites. Passionate Web Developer with skills in
+              the MERN stack.I'm skilful in problem-solving and building new
+              things using technology. I look forward to joining a company where
+              I can contribute to individual and company growth..
             </p>
             <div>
               <img
