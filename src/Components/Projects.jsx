ceb5222441ef2f0197411fdc154a9933c24099c0
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-01-03 19:34:31 +0530

    added

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,66 +2,66 @@ import React from "react";
 import "../CSS/Projects.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import StreamIcon from "@mui/icons-material/Stream";
-import ProjectImage from "../Images/Project/Booking.PNG";
-import Html from "../Images/SkillImages/Html.png";
-import Css from "../Images/SkillImages/Css.png";
-import JavaScript from "../Images/SkillImages/JavaScript.png";
-import Git from "../Images/SkillImages/git.png";
-import Github from "../Images/SkillImages/Github.png";
-import ReactPng from "../Images/SkillImages/react.png";
-import Redux from "../Images/SkillImages/redux.png";
-import ChakraUi from "../Images/SkillImages/ChakraUi.png";
-import Mongodb from "../Images/SkillImages/mongodb.png";
-import Nodejs from "../Images/SkillImages/nodejs.png";
-import Mern from "../Images/SkillImages/mern.png";
-import Express from "../Images/SkillImages/Express.png";
-import Nextjs from "../Images/SkillImages/nextjs.png";
-import Typescript from "../Images/SkillImages/ts.png";
+import BookingImage from "../Images/Project/Booking.PNG";
+
+// import ProjectsData from "../Data/ProjectData.js";
+
+const ProjectsData = () => [
+  {
+    id: 1,
+    title: "Booking Clone",
+    img: BookingImage,
+    desc: "Booking.com, headquartered in Amsterdam, is one of the largest online travel agencies which is operating on online Hotels, Flights, Tour pacakges etc.",
+    githubLink: "https://github.com/MoHitksS/organic-market",
+    deployedLink: "https://organicmarket.netlify.app/",
+    tags: [
+      "React",
+      "Material UI",
+    ],
+  },
+];
 
 const Projects = () => {
   return (
     <div className="ProjectMain">
       <h1 className="ProjectH1">Projects</h1>
-      <div className="ProjectSection">
-        <div className="ProjectLeftSection">
-          <h2 className="ProjectName">01. Booking</h2>
-          <div className="ProjectLeftGreen">
-            <p style={{ color: "white" }}>
-              Booking.com, headquartered in Amsterdam, is one of the largest
-              online travel agencies which is operating on online Hotels,
-              Flights, Tour pacakges etc.
-            </p>
-          </div>
-          {/* <p className="PTagTechStack">TechStack </p> */}
-          <div className="TechStack">
-            <div className="TechStackContainer">
-              <p>ReactJS</p>
-              <img className="TechStackImages" src={ReactPng} alt="" />
+      {/* {ProjectsData.map((el) => ( */}
+        <div className="ProjectSection">
+          <div className="ProjectLeftSection">
+            <h2 className="ProjectName">01. Booking</h2>
+            <div className="ProjectLeftGreen">
+              <p style={{ color: "white" }}>
+                Booking.com, headquartered in Amsterdam, is one of the largest
+                online travel agencies which is operating on online Hotels,
+                Flights, Tour pacakges etc.
+              </p>
             </div>
+
             <div className="TechStackContainer">
-              <p>HTML</p>
-              <img className="TechStackImages" src={Html} alt="" />
+              <p className="PTagForSKills">React</p>
+              <p className="PTagForSKills">React</p>
+              <p className="PTagForSKills">React</p>
+            </div>
+            <div className="linkSection">
+              <a href="">
+                <button className="ProjectButtons">
+                  <GitHubIcon />
+                  <p style={{ marginLeft: "0.3rem" }}>Github</p>
+                </button>
+              </a>
+              <a href="">
+                <button className="ProjectButtons">
+                  <StreamIcon />
+                  <p style={{ marginLeft: "0.3rem" }}>Live</p>
+                </button>
+              </a>
             </div>
           </div>
-          <div className="linkSection">
-            <a href="">
-              <button style={{fontWeight:'600'}}>
-                <GitHubIcon />
-                Github
-              </button>
-            </a>
-            <a href="">
-              <button style={{fontWeight:'600'}}>
-                <StreamIcon />
-                Live
-              </button>
-            </a>
+          <div className="ProjectRightSection">
+            <img className="ProjectImages" src={BookingImage} alt="" />
           </div>
         </div>
-        <div className="ProjectRightSection">
-          <img className="ProjectImages" src={ProjectImage} alt="" />
-        </div>
-      </div>
+      {/* ))} */}
     </div>
   );
 };
---
+++ src/Data/ProjectData.js
@@ -0,0 +1,18 @@
+import BookingImage from "../Images/Project/Booking.PNG";
+
+const ProjectsData = () => [
+  {
+    id: 1,
+    title: "Booking Clone",
+    img: BookingImage,
+    desc: "Booking.com, headquartered in Amsterdam, is one of the largest online travel agencies which is operating on online Hotels, Flights, Tour pacakges etc.",
+    githubLink: "https://github.com/MoHitksS/organic-market",
+    deployedLink: "https://organicmarket.netlify.app/",
+    tags: [
+      "React",
+      "Material UI",
+    ],
+  },
+];
+
+export default ProjectsData
