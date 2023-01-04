e6beec192f63f9db3b65cd9038b1e006a065c4ec
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-01-05 02:42:52 +0530

    Update Home.jsx

--- src/AllRoutes/Home.jsx
+++ src/AllRoutes/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../Components/About";
 import Contact from "../Components/Contact";
+import Footer from "../Components/Footer";
 import Github from "../Components/Github";
 import Navbar from "../Components/Navbar";
 import Projects from "../Components/Projects";
@@ -15,6 +16,7 @@ const Home = () => {
       <Projects />
       <Github />
       <Contact />
+      <Footer/>
     </>
   );
 };
