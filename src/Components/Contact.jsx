f0d6917555f4a0e5fcd1ffec89a3bff53b75fc39
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-01-23 10:58:03 +0530

    adding twitter ✌

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,7 @@ import "../CSS/Contact.css";
 import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
+import TwitterIcon from '@mui/icons-material/Twitter';
 const Contact = () => {
   return (
     <div id="about" className="ContactContainer">
@@ -23,6 +23,14 @@ const Contact = () => {
                 target="_blank"
               >
                 <p>Nitin Sharma</p>
+              </a>
+            </div>
+            <div>
+              <TwitterIcon fontSize="large" />
+              <h1>Twitter</h1>
+              <p>You can contact me on Twitter</p>
+              <a href="https://twitter.com/ignitinsharma" rel="noreferrer" target="_blank">
+                <p>@ignitinsharma</p>
               </a>
             </div>
             <div>
