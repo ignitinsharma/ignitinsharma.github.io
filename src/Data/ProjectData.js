d74d63092b08b79a3f1ef42e1b8404601e036397
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-03-01 15:53:26 +0530

    adding project

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,35 +1,44 @@
 import BookingImage from "../Images/Project/Booking.PNG";
-import BluemercuryImage from "../Images/Project/Bluemercury.PNG";
+import Clotheyard from "../Images/Project/clotheyard.png";
 import BumBlebee from "../Images/Project/zoomcar.png";
 
 const ProjectsData = [
   {
     id: 1,
-    title: "01. bumBlebee",
+    title: "01. Clothyard",
+    img: Clotheyard,
+    desc: "Clotheyard, is an shopping mall–based retailer of casual apparel and accessories, principally targeting young adults and teenagers. This project build by team of 5 members.",
+    githubLink: "https://github.com/PriyanshuPatil/Clotheyard.com",
+    deployedLink: "https://clotheyard1.netlify.app/",
+    tags: ["React", "Redux", "Chakraui", "MongoDB", "ExpressJs", "Nodejs"],
+  },
+  {
+    id: 2,
+    title: "02. bumBlebee",
     img: BumBlebee,
     desc: "BumBlebee is a clone of zoomcar website which is car rental website. This project build by team of 4 members.",
     githubLink: "https://github.com/Suneet25/zoomcar-clone",
     deployedLink: "https://sick-insect-8581.vercel.app/",
-    tags: ["Html", "Css", "React", "Chakra-ui"],
+    tags: ["React", "Redux", "Chakra-ui"],
   },
   {
-    id: 2,
-    title: "02. Booking Clone",
+    id: 3,
+    title: "03. Booking Clone",
     img: BookingImage,
     desc: "Booking.com, headquartered in Amsterdam, is one of the largest online travel agencies which is operating on online Hotels, Flights, Tour pacakges etc.",
     githubLink: "https://github.com/ignitinsharma/Booking.com-Clone",
     deployedLink: "https://mybooking-com.vercel.app/",
     tags: ["Html", "Css", "React", "Mui Icons"],
   },
-  {
-    id: 2,
-    title: "03. Bluemercury Clone",
-    img: BluemercuryImage,
-    desc: "Bluemercury.com is a leading luxury beauty retailer offering the best cosmetics, skincare, makeup, perfume, hair, bath, and body. This project build by team of 4 members.",
-    githubLink: "https://github.com/ignitinsharma/bluemercury_clone",
-    deployedLink: "https://bluemercury-clone-project.netlify.app/",
-    tags: ["Html", "Css", "JavaScript"],
-  },
+  // {
+  //   id: 3,
+  //   title: "03. Bluemercury Clone",
+  //   img: BluemercuryImage,
+  //   desc: "Bluemercury.com is a leading luxury beauty retailer offering the best cosmetics, skincare, makeup, perfume, hair, bath, and body. This project build by team of 4 members.",
+  //   githubLink: "https://github.com/ignitinsharma/bluemercury_clone",
+  //   deployedLink: "https://bluemercury-clone-project.netlify.app/",
+  //   tags: ["Html", "Css", "JavaScript"],
+  // },
 ];
 
 export default ProjectsData;
--- src/Data/SecondryPojectsData.js
+++ src/Data/SecondryPojectsData.js
@@ -2,10 +2,20 @@ import TodoImage from "../Images/Project/MyTodo.PNG";
 import YoutubeImage from "../Images/Project/Youtube.PNG";
 import WeatherImage from "../Images/Project/Weather.PNG";
 import EnvoyImage from "../Images/Project/Envoy.PNG";
+import BluemercuryImage from "../Images/Project/Bluemercury.PNG";
 
 const SecondryPojects = [
   {
     id: 1,
+    title: "Bluemercury Clone",
+    img: BluemercuryImage,
+    desc: "BumBlebee is a clone of zoomcar website which is car rental website. This project build by team of 4 members.",
+    githubLink: "https://github.com/ignitinsharma/bluemercury_clone",
+    deployedLink: "https://bluemercury-clone-project.netlify.app/",
+    tags: ["Html", "Css", "JavaScript"],
+  },
+  {
+    id: 2,
     title: "Todo App",
     img: TodoImage,
     desc: "Basic Todo App here you can plan your work.",
@@ -14,7 +24,7 @@ const SecondryPojects = [
     tags: ["Html", "Css", "React"],
   },
   {
-    id: 2,
+    id: 3,
     title: "Youtube Clone",
     img: YoutubeImage,
     desc: "Basic clone of Youtube using fetch APi method.",
@@ -23,7 +33,7 @@ const SecondryPojects = [
     tags: ["Html", "Css", "Javascript"],
   },
   {
-    id: 3,
+    id: 4,
     title: "Weather App",
     img: WeatherImage,
     desc: "Build weather showing web app using fetch and Openweather API",
@@ -32,8 +42,8 @@ const SecondryPojects = [
     tags: ["Html", "Css", "Javascript"],
   },
   {
-    id: 4,
-    title: "03. Envoy Clone",
+    id: 5,
+    title: "Envoy Clone",
     img: EnvoyImage,
     desc: "Improve workplace experiences with Envoy visitor management, employee sign in, desk reservations, room booking, and delivery management software. This project build by team of 5 members.",
     githubLink: "https://github.com/ignitinsharma/Envoy-Clone",
